05782f140a50b42794891abebe79f649c232e1ff
somtodev <leroynnalue65@gmail.com>
2024-04-15 22:17:34 +0100

    Update: + Madre | Elegy For My Brave Mom

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,6 +4,7 @@ const blogCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     description: z.string(),
+    cover: z.string().default("/covers/default.png"),
     date: z.string().transform((date) => new Date(date)),
     keywords: z.string().default("somtochukwu, blog"),
     draft: z.boolean().default(false),
